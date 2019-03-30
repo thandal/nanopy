62364d0c1ca842aed1d9223a38df43a32603250f
128 <42086377+nano128@users.noreply.github.com>
2019-03-30 20:43:16 +0100

    ed25519-donna checkvalid

--- nanopy/ed25519_blake2b.c
+++ nanopy/ed25519_blake2b.c
@@ -41,16 +41,26 @@ static PyObject *publickey(PyObject *self, PyObject *args) {
 static PyObject *signature(PyObject *self, PyObject *args) {
   const unsigned char *m, *sk, *pk;
   int i = 0, j = 0, k = 0;
-  ed25519_signature sign;
+  ed25519_signature sig;
 
   if (!PyArg_ParseTuple(args, "y#y#y#", &m, &i, &sk, &j, &pk, &k)) return NULL;
-  ed25519_sign(m, i, sk, pk, sign);
-  return Py_BuildValue("y#", &sign, 64);
+  ed25519_sign(m, i, sk, pk, sig);
+  return Py_BuildValue("y#", &sig, 64);
 }
 
-static PyMethodDef m_methods[] = {{"publickey", publickey, METH_VARARGS, NULL},
-                                  {"signature", signature, METH_VARARGS, NULL},
-                                  {NULL, NULL, 0, NULL}};
+static PyObject *checkvalid(PyObject *self, PyObject *args) {
+  const unsigned char *sig, *m, *pk;
+  int i = 0, j = 0, k = 0;
+
+  if (!PyArg_ParseTuple(args, "y#y#y#", &sig, &i, &m, &j, &pk, &k)) return NULL;
+  return Py_BuildValue("i", ed25519_sign_open(m, j, pk, sig));
+}
+
+static PyMethodDef m_methods[] = {
+    {"publickey", publickey, METH_VARARGS, NULL},
+    {"signature", signature, METH_VARARGS, NULL},
+    {"checkvalid", checkvalid, METH_VARARGS, NULL},
+    {NULL, NULL, 0, NULL}};
 
 static struct PyModuleDef ed25519_blake2b_module = {
     PyModuleDef_HEAD_INIT, "ed25519_blake2b", NULL, -1, m_methods};
