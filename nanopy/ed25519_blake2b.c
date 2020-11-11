d88cb92324c19f29a8ec3a3e0f4f97d429e98d41
npy <42086377+npy0@users.noreply.github.com>
2020-11-11 11:18:18 +0100

    ed25519 f89e42a

--- nanopy/ed25519_blake2b.c
+++ nanopy/ed25519_blake2b.c
@@ -7,20 +7,17 @@
 
 void ed25519_randombytes_unsafe(void *out, size_t outlen) {}
 
-blake2b_state b2b;
-
 void ed25519_hash_init(ed25519_hash_context *ctx) {
-  ctx->blake2 = &b2b;
-  blake2b_init(ctx->blake2, 64);
+  blake2b_init(ctx, 64);
 }
 
 void ed25519_hash_update(ed25519_hash_context *ctx, uint8_t const *in,
                          size_t inlen) {
-  blake2b_update(ctx->blake2, in, inlen);
+  blake2b_update(ctx, in, inlen);
 }
 
 void ed25519_hash_final(ed25519_hash_context *ctx, uint8_t *out) {
-  blake2b_final(ctx->blake2, out, 64);
+  blake2b_final(ctx, out, 64);
 }
 
 void ed25519_hash(uint8_t *out, uint8_t const *in, size_t inlen) {
